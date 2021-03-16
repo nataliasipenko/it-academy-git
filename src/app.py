52397458400874f00f11019eeeb1482ca692cbac
natalia.mashliakevich <natalia.asipenko@gmail.com>
2021-03-16 17:41:07 +0300

    Fix typo in greeting

--- src/app.py
+++ src/app.py
@@ -1,7 +1,7 @@
-#0._GIT._Домашняя_работа
+# 0._GIT._Домашняя_работа
 
 print("I’m planning to learn git conflicts resolution")
 name = input("Please enter your name: ")
 surname = input("Please enter your surname: ")
 nickname = input("Please enter your nickname ")
-print("Hi, {} {} a.k.a. {}!’".format(name, surname, nickname))
+print("Hi, {} {} a.k.a. {}!".format(name, surname, nickname))
