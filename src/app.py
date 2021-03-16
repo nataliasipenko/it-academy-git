c4663da90d1b997d8518a198d4314fcd3007b68b
natalia.mashliakevich <natalia.asipenko@gmail.com>
2021-03-16 16:09:52 +0300

    Enter name, surname and display a greeting

--- src/app.py
+++ src/app.py
@@ -1,3 +1,6 @@
 #0._GIT._Домашняя_работа
 
 print("I’m planning to learn git conflicts resolution")
+name = input("Please enter your name: ")
+surname = input("Please enter your surname: ")
+print("Hi, {} {}!’".format(name, surname))
